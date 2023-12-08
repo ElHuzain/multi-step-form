14b534f89352790c87ebc2470d6ffb740f629d47
Ahmed Huzain <a.huzain98@gmail.com>
2023-12-08 23:46:15 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button';
+import './styles/Global.css';
+
+function App() {
+  return (
+    <div className="">
+      <button className="">1wefwef</button>
+      <h1 className="text-primary-1 font-bold">Personal Info</h1>
+      <Button>Ye!</Button>
+    </div>
+  )
+}
+
+export default App

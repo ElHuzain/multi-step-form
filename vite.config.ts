816aa8bbc775badcd8b6e4e1533154496eef51c9
Ahmed Huzain <a.huzain98@gmail.com>
2023-12-13 20:30:33 +0200

    fixing images

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      $bgs: path.resolve(__dirname, "./public/images/bg-sidebar-desktop.svg")
     },
   },
 })
